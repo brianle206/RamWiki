15a58a3e884f49eee5b8e649e974d5311a92133e
Brian Le <brianle206@gmail.com>
2016-08-03 09:28:28 -0700

    added user

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,15 +1,17 @@
 class UserController < ApplicationController
   def index
+  	@user = User.find(session[:user_id])
   end
   def create_landing
   end
   def create
  
-	@user = User.create(first_name:params[:first_name], last_name:params[:last_name], email:params[:email], password:params[:password])
-	if @user.save == true
-		redirect_to "/user/index/#{@user.id}"
+	user = User.create(first_name:params[:first_name], last_name:params[:last_name], email:params[:email], password:params[:password])
+	if user.save == true
+		session[:user_id] = user.id
+		redirect_to "/user/index/#{user.id}"
 	else
-  		flash[:errors] = @user.errors.full_messages
+  		flash[:errors] = user.errors.full_messages
   		redirect_to '/'
   	  	
   	end
